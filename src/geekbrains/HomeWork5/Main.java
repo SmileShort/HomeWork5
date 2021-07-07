126398922df8baa4c70cef53a2402545b992596a
SmileShort <86470906+SmileShort@users.noreply.github.com>
2021-07-07 12:33:17 +0500

    Initial commit

---
+++ src/geekbrains/HomeWork5/Main.java
@@ -0,0 +1,29 @@
+package geekbrains.HomeWork5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Personal[] persArray = new Personal[5];
+        persArray[0] = new Personal("Ivanov Ivan", "Product manager", "IvanovII@rambler.ru", "89509630101", 70000, 45);
+        persArray[1] = new Personal("Petrov Petr", "Manager", "PetrovPP@rambler.ru", "89509630102", 60000, 35);
+        persArray[2] = new Personal("Sidorov Nikolay", "Ingineer", "SidorovNK@rambler.ru", "89509630103", 30000, 27);
+        persArray[3] = new Personal("Mashkina Marina", "Human Resources Specialist", "MashkinaMA@rambler.ru", "89509630104", 45000, 41);
+        persArray[4] = new Personal("Fedorova Antonina", "Accountant", "FedorovaAP@rambler.ru", "89509630105", 35000, 39);
+
+        System.out.println("Введите интересующий Вас возраст: ");
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+
+        System.out.println("Данные сотрудники старше " + year + " лет: ");
+        for (int i = 0; i < persArray.length; i++){
+            if (persArray[i].age > year){
+                persArray[i].info();
+            }
+        }
+
+    }
+}
---
+++ src/geekbrains/HomeWork5/Personal.java
@@ -0,0 +1,28 @@
+package geekbrains.HomeWork5;
+
+public class Personal {
+
+    String fullName;
+    String position;
+    String email;
+    String phone;
+    int salary;
+    int age;
+
+    Personal (String fullName, String position, String email, String phone, int salary, int age){
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void info(){
+        System.out.println("ФИО: " + this.fullName + " Должность: " + this.position + " Почта: " + this.email + " Телефон: " + this.phone + " Зарплата: " + this.salary + " Возраст: " + this.age);
+    }
+
+
+
+
+}
